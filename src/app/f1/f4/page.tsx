c614560447372b8d22ae81bf3e2b0dcecc57c327
Musha02 <89347202+Musha02@users.noreply.github.com>
2024-04-23 01:58:03 +0530

    add intercepting routes

--- src/app/f1/f4/page.tsx
+++ src/app/f1/f4/page.tsx
@@ -3,8 +3,11 @@ import Link from "next/link"
 export default function F4() {
     return (
         <>
-            <h1>F4 page</h1>; 
-            <div><Link href="/f1/f3">F3</Link> </div>
+            <h1>F4 page</h1>
+            <div>
+                <Link href="/f1/f3">F3</Link> 
+                <Link href="/about">About</Link>
+            </div>
         </>
     );
 }
